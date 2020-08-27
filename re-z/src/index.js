402319230b0e5760d88ba00a350ac96d85296c3a
Gerardo Garavito <garavitogerardo@gmail.com>
2020-08-27 18:02:49 -0500

    added the global.css

--- re-z/src/index.js
+++ re-z/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './global.css';
 
 import Button from './components/Button'
 import RecipeCardHorizontalSimple from './components/RecipeCardHorizontalSimple'
+
 // ---las librerías de abajo venían con create-react-app, aun no las sé usar entonces las comenté---
-//import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
