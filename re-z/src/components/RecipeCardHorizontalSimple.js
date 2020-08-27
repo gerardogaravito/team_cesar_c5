843f47fb8488ed8076c6df2e97552f7ee60859c2
Gerardo Garavito <garavitogerardo@gmail.com>
2020-08-27 15:15:56 -0500

    added the styles directory and added css to the recipe component

--- re-z/src/components/RecipeCardHorizontalSimple.js
+++ re-z/src/components/RecipeCardHorizontalSimple.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import fotoComida from '../images/fotoComida.jpg';
+import "./styles/RecipeCardHorizontalSimple.css" // traer los estilos
+import fotoComida from '../images/fotoComida.jpg'; //se trae la imagen y la guarda en la variable fotoComida
 
 class RecipeCardHorizontalSimple extends React.Component {
   render() {
-    return <div>
-      <img src={fotoComida} alt="Foto de Comida"/>
-      <h1>Lorem Ipsum 36</h1>
+    return <div className='card'>
+      <img className='card__image' src={fotoComida} alt="Foto de Comida"/>
+      <div className='banner'>
+        <h1 className='banner__title'>Lorem Ipsum 36</h1>
+      </div>
     </div>
   }
 }
