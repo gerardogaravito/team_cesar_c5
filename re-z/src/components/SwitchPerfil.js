e34a8315b8fa3fedeb04a34b0cc9ac6b6ab4e6dd
Hector Remedios <hectoremedio90@gmail.com>
2020-08-28 03:46:30 -0500

    the switch have animation

--- re-z/src/components/SwitchPerfil.js
+++ re-z/src/components/SwitchPerfil.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './styles/SwitchPerfil.css';
 /* import Avatar from '../images/Atomos/Avatar.svg' */
 /* import logoFacebook from '../images/Atomos/Facebook-icon.svg';
 import logoTwitter from '../images/Atomos/Twitter-icon.svg'; */
 
+class Modal extends Component{
+    render(){
+        return(
+          <div className="perfilMenu">
+              <a href="">Configuración</a>
+              <a href="">Tus Favoritas</a>
+              <a href="">Ver carrito de compra</a>
+              <a href="">Cerrar sesión</a>
+          </div>
+        )
+    }
+}
+
 class SwitchPerfil extends React.Component {
+    state = {isModalOpen: false}
+    _openModal = () => this.setState({isModalOpen: true})
+    _closeModal = () => this.setState({isModalOpen: false})
+
     render() {
       return (
-         <div className="SwtchContainer">
-              <input type="checkbox" name=""></input>
-            <div className="perfilMenu">
-                <a href="">Configuración</a>
-                <a href="">Tus Favoritas</a>
-                <a href="">ver carrito de compra</a>
-                <a href="">Cerrar sesión</a>
-            </div>
-         </div>
-         
-        
+         <level className="SwtchContainer">
+              <input type="checkbox" name="" onClick={this._openModal}></input>
+            {this.state.isModalOpen && 
+                <Modal onClose={this._closeModal}/>
+            }
+         </level>   
       )     
     }
   }
   
   
+
   export default SwitchPerfil;
+
+
+  {/* <img src={Avatar} alt="Avatar"/> */}
