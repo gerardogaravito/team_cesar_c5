ad2587e5d8fc3357133307a8a9368a29e9e22393
Jose Garzon <jd.garzon12@gmail.com>
2020-08-28 15:11:02 -0500

    recipe carrousel created

--- walle/src/index.js
+++ walle/src/index.js
@@ -8,8 +8,9 @@ import ReactDOM from "react-dom";
 // import SearchHero from "./components/Com--Search-hero";
 // import Footer from "./components/Com--Footer";
 // import RecipeDashboard from "./components/Com--Recipes-dashboard";
-import IngredientCarrousel from "./components/Com--Ingredient-carrousel";
+// import IngredientCarrousel from "./components/Com--Ingredient-carrousel";
+import RecipeCarrousel from "./components/Com--Recipe-carrousel";
 
 //------------------------------ import styles and images
 
-ReactDOM.render(<IngredientCarrousel />, document.getElementById("root"));
+ReactDOM.render(<RecipeCarrousel />, document.getElementById("root"));
