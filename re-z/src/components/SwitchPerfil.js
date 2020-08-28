f2a807ad7431846237e196af457bda5d3b4834e5
Hector Remedios <hectoremedio90@gmail.com>
2020-08-28 01:58:45 -0500

    styles in the switch component (Perfil-button)

--- re-z/src/components/SwitchPerfil.js
+++ re-z/src/components/SwitchPerfil.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './styles/SwitchPerfil.css';
-import Avatar from '../images/Atomos/Avatar.svg'
+/* import Avatar from '../images/Atomos/Avatar.svg' */
 /* import logoFacebook from '../images/Atomos/Facebook-icon.svg';
 import logoTwitter from '../images/Atomos/Twitter-icon.svg'; */
 
@@ -9,11 +9,19 @@ class SwitchPerfil extends React.Component {
     render() {
       return (
          <div className="SwtchContainer">
-             <img src={Avatar} alt="Avatar"/>
-             <input type="checkbox" name=""></input>
+              <input type="checkbox" name=""></input>
+            <div className="perfilMenu">
+                <a href="">Configuración</a>
+                <a href="">Tus Favoritas</a>
+                <a href="">ver carrito de compra</a>
+                <a href="">Cerrar sesión</a>
+            </div>
          </div>
+         
+        
       )     
     }
   }
+  
   
   export default SwitchPerfil;
