e6dfb596188bf0fef0f95b83f1c26c111b2ecf44
Jose Garzon <jd.garzon12@gmail.com>
2020-08-29 12:34:41 -0500

    metadata component created

---
+++ walle/src/cards/Card--Metadata.js
@@ -0,0 +1,37 @@
+//------------------------------ import libraries
+import React, { Fragment } from "react";
+
+//------------------------------ import components
+import { FaClock, FaUsers, FaFire } from "react-icons/fa";
+
+//------------------------------ import styles and images
+import "./styles/Metadata.scss";
+
+const Metadata = (props) => {
+  return (
+    <Fragment>
+      <div className="Metadata">
+        <div className="Metadata__item">
+          <FaClock className="icon--active" />
+          <p>
+            Tiempo <br /> {props.time} Min
+          </p>
+        </div>
+        <div className="Metadata__item">
+          <FaUsers className="icon--active" />
+          <p>
+            Porciones <br /> {props.portions} personas
+          </p>
+        </div>
+        <div className="Metadata__item">
+          <FaFire className="icon--active" />
+          <p>
+            Dificultad <br /> {props.difficult}
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Metadata;
