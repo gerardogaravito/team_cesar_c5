73ce754045a93f194d57ee0b01fd194738e82e30
Hector Remedios <hectoremedio90@gmail.com>
2020-08-28 19:15:05 -0500

    Change in de perfil's button

--- re-z/src/components/SwitchPerfil.js
+++ re-z/src/components/SwitchPerfil.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 
 import './styles/SwitchPerfil.css';
-/* import Avatar from '../images/Atomos/Avatar.svg' */
-/* import logoFacebook from '../images/Atomos/Facebook-icon.svg';
-import logoTwitter from '../images/Atomos/Twitter-icon.svg'; */
+import CloseBtn from "../images/Atomos/close-icon.svg";
+import Avatar from "../images/Atomos/Avatar.svg";
 
 class Modal extends Component{
     render(){
         return(
+            <section>
+            <div className="close-btn">
+                <button onClick={this.props.onClose}>
+                    <img src={CloseBtn} alt="Close-Button"/>
+                </button>
+            </div>
           <div className="perfilMenu">
               <a href="">Configuración</a>
               <a href="">Tus Favoritas</a>
               <a href="">Ver carrito de compra</a>
               <a href="">Cerrar sesión</a>
           </div>
+          </section>
         )
     }
 }
@@ -26,18 +32,23 @@ class SwitchPerfil extends React.Component {
     render() {
       return (
          <level className="SwtchContainer">
-              <input type="checkbox" name="" onClick={this._openModal}></input>
+
+            <div className="perfil-btn" onClick={this._openModal}>
+                <img src={Avatar} alt="Avatar"/>
+            </div>
             {this.state.isModalOpen && 
                 <Modal onClose={this._closeModal}/>
             }
+           
          </level>   
       )     
     }
   }
-  
-  
-
-  export default SwitchPerfil;
 
 
-  {/* <img src={Avatar} alt="Avatar"/> */}
+
+ export default SwitchPerfil;
+/*  <input type="checkbox" name="" onClick={this._openModal}></input>
+ {this.state.isModalOpen && 
+     <Modal onClose={this._closeModal}/>
+ } */
--- re-z/src/index.js
+++ re-z/src/index.js
@@ -6,8 +6,9 @@ import './global.css';
 import SwitchPerfil from "./components/SwitchPerfil"
 import RecipeCardHorizontalSimple from './components/RecipeCardHorizontalSimple'
 /* import FooterA from './components/FooterA' */
-import RecipeCardHorizontalDerecha from './components/RecipeCardHorizontalDerecha'
+/* import RecipeCardHorizontalDerecha from './components/RecipeCardHorizontalDerecha'
 import Switch from './components/SwitchPerfil';
+import PerfilSWitch from './components/Perfil'; */
 
 // ---las librerías de abajo venían con create-react-app, aun no las sé usar entonces las comenté---
 // import App from './App';
@@ -19,6 +20,7 @@ const container = document.getElementById('app');
 
 // ReactDOM.render( qué, dónde)
 // ReactDOM.render( <elemento/>, dónde)
+/* ReactDOM.render(<SwitchPerfil/>, container); */
 ReactDOM.render(<SwitchPerfil/>, container);
 /* ReactDOM.render(<Button/>, container); */
 /* ReactDOM.render(<FooterA/>, container); */
