6b62cdd12b9436a3cbe896428db260c254412ff5
Gerardo Garavito <garavitogerardo@gmail.com>
2020-09-02 16:10:19 -0500

    organize the index.js

--- re-z/src/index.js
+++ re-z/src/index.js
@@ -1,33 +1,33 @@
+//DEPENDENCIAS
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//COMPONENTES
 import './global.css';
 import RecipeCardSquare from './components/RecipeCardSquare'
-/* import Button from './components/Button' */
+//import Button from './components/Button' */
 //import SwitchPerfil from "./components/SwitchPerfil"
-import RecipeCardHorizontalSimple from './components/RecipeCardHorizontalSimple'
-/* import FooterA from './components/FooterA' */
-/* import RecipeCardHorizontalDerecha from './components/RecipeCardHorizontalDerecha'
-import Switch from './components/SwitchPerfil';
- */
-import Search from './components/Search'
-// ---las librerías de abajo venían con create-react-app, aun no las sé usar entonces las comenté---
+//import RecipeCardHorizontalSimple from './components/RecipeCardHorizontalSimple'
+//import FooterA from './components/FooterA' */
+//import RecipeCardHorizontalDerecha from './components/RecipeCardHorizontalDerecha'
+//import Switch from './components/SwitchPerfil';*/
+//import Search from './components/Search'
+
+//Para cuando queramos renderiar todo el sitio con App
 // import App from './App';
+
+// ---las librerías de abajo venían con create-react-app, aun no las sé usar entonces las comenté---
 // import * as serviceWorker from './serviceWorker';
 
 
+const container = document.getElementById('app');
 
-//const container = document.getElementById('app');
 
 // ReactDOM.render( qué, dónde)
 // ReactDOM.render( <elemento/>, dónde)
 
-/* ReactDOM.render(<SwitchPerfil/>, container); */
-/* ReactDOM.render(<Button/>, container); */
-/* ReactDOM.render(<FooterA/>, container); */
-//ReactDOM.render(<Search/>, container);
 
-const container = document.getElementById('app');
-
-// ReactDOM.render(---qué queremos renderizar--,
-//--y donde lo queremos hacer--);
 ReactDOM.render(<RecipeCardSquare />,container)
+
+//Para cuando queramos renderizar todo el sitio con App.js
+// ReactDOM.render(<App />,container)
