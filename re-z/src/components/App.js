ab60b483e75a0df6b9dc9ff1290cb2fcedba10c3
Gerardo Garavito <garavitogerardo@gmail.com>
2020-09-04 13:30:46 -0500

    added the error 404 page

--- re-z/src/components/App.js
+++ re-z/src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from "./page/Home";
 import Favoritos from "./page/Favoritos";
 import Landing from './page/Landing';
+import NotFound from './page/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/home/favoritos" component={Favoritos} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
---
+++ re-z/src/components/page/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <h2>Error 404</h2>
+        <h3>No pudimos encontrar lo que buscabas</h3>
+      </React.Fragment>
+    )
+  }
+}
+
+export default NotFound;
