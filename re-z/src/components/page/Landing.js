29f0528da8b945e01f515b6ae123e64e2d7b7319
Hector Remedios <hectoremedio90@gmail.com>
2020-09-05 22:49:17 -0500

    landing page setup with video and modal

--- re-z/src/components/page/Landing.js
+++ re-z/src/components/page/Landing.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 
 import '../styles/Landing.css';
 import NavBarLanding from '../NavBarLanding'
 import CardVerticalScroll from '../CardVerticalScroll';
 import FooterA from '../FooterA'
-
-import spot from '../../images/spotPrueba.png'
+import RecipeCardSquare from '../RecipeCardSquare';
 
 class Landing extends React.Component {
   render () {
@@ -14,13 +14,23 @@ class Landing extends React.Component {
               <header>
                 <NavBarLanding/>
               </header>
-              <div className="landing-container">
+              <div className="landing-container-principal">
                 <div className="landing__spot">
-                  <img src={spot} alt="spot"/>
+                  <ReactPlayer url='https://youtu.be/kuB8rR2b2Ak'
+                  width='100%'
+                  height='100%'
+                  playing= 'true' 
+                  loop
+                  muted/>
                 </div>
                 <div className="landing__recipes">
                   <CardVerticalScroll />
                 </div>
+              </div>
+
+              <div className="landing__container-secundary">
+                {/* Dentro de este div debe de ir el contenido que sigue al video y al escroll de las tarjetas el llamado de la siguiente tarjeta es solo contenido de relleno*/}
+                <RecipeCardSquare />
               </div>
               <FooterA/>
         </div>
