c326f1e71e42c263335d0b10dc78621564511fec
Gerardo Garavito <garavitogerardo@gmail.com>
2020-09-09 11:11:31 -0500

    change the static button to a dynamic button

--- re-z/src/components/Button.js
+++ re-z/src/components/Button.js
@@ -1,12 +1,15 @@
-import React from 'react'; //se usa la librería react porque escribiremos jsx
+import React from 'react';
 
-import './styles/Button.css'
+import './styles/Button.css';
 
-//los componentes son clases, por ello, class Button; y extiende a la clase React.Component
-class Button extends React.Component {
-  render() { //todos los componentes requieren almenos este método, define el resultado que se verá en pantalla
-    return <button className='button'>Button 20</button>;
-  }
+
+const Button = ({text}) => {
+
+  return (
+    <button 
+      className='button'
+    >{text}</button>
+  )
 }
 
-export default Button //hay que exportar el archivo/componente
+export default Button;
