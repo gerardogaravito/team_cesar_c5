c93534a0d5e8e80343e31e738499dd914c729072
Hector Remedios <hectoremedio90@gmail.com>
2020-09-12 15:58:07 -0500

    merge component crated for Jose to my local repo..

--- re-z/src/components/RecipeCardSquareSimple.js
+++ re-z/src/components/RecipeCardSquareSimple.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
 import './styles/RecipeCardSquareSimple.css'
 import fotoComida6 from '../images/fotoComida6.png';
 
@@ -8,7 +7,7 @@ const RecipeCardSquareSimple=({name}) => {
             return (
                 <Link to="/">
                 <div className='cardSquareSimple'>
-                    <img src={fotoComida6} alt="foto de comida"/>
+                   <img src={fotoComida6} alt="foto de comida"/>
                     <h3 className="cardSquareSimple__title">{name}</h3>
                 </div>
                 </Link>
