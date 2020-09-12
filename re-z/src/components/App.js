4866cc2b2dfb5aec437154bea254c24bff20680e
jose caicedo <joseluiscl@gmail.com>
2020-09-11 21:14:56 -0500

    repeating files

--- re-z/src/components/App.js
+++ re-z/src/components/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-
-import RecipeCardSquareSimple from './RecipeCardSquareSimple';
-import RecipeCardVerticalSimple from './RecipeCardVerticalSimple'
+import Home from "./page/Home";
+import Favoritos from "./page/Favoritos";
+import Landing from './page/Landing';
+import NotFound from './page/NotFound';
+import FooterA from './FooterA';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <RecipeCardSquareSimple name="Lorem Ipsum 36"/>
-        <RecipeCardVerticalSimple name="Lorem Ipsum 36"/>
-      </div>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/home/favoritos" component={Favoritos} />
+        <Route exact path="/home/Close" component={Landing} />
+        <Route component={NotFound} />
+      </Switch>
+      {/* <FooterA /> */}
     </BrowserRouter>
   );
 }
-
-export default App;
