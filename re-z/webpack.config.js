17af49b2ec3f8889f25dd1430bc82db88a06e926
Hector Remedios <hectoremedio90@gmail.com>
2020-09-16 23:14:43 -0500

    correction in webpack.dev for hot updates

--- re-z/webpack.config.js
+++ re-z/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].[hash].js',
-    //publicPath: 'http://localhost:3001/',
+    publicPath: './',
     chunkFilename: 'js/[id].[chunkhash].js'
   },
   optimization: {
