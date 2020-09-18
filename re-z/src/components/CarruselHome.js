28fab4c9ea494f177d5c7160177402bb59f842c9
Hector Remedios <hectoremedio90@gmail.com>
2020-09-18 02:46:10 -0500

    optimasation of carrusel components

--- re-z/src/components/CarruselHome.js
+++ re-z/src/components/CarruselHome.js
@@ -1,52 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './styles/CarruselHome.css';
-import PhotoFood from '../images/Photo-food.png'
+import CarruselItems from './CarruselItems'
 
 
-class CarruselHome extends React.Component {
+class CarruselHome extends Component {
 
-    render() {
-
+  render() {
       return (
         <section className="Principal-container">
         <div className="Triangle"></div>
           <section className="carousel">
             <div className="carousel__container">
-              <div className="carousel-item">
-                <img src={PhotoFood} alt="" className="carousel-item__img"/>
-                <div class="carousel-item__details">
-                <div className="like-box">
-                  <div className="like" id="like"></div>
-                </div>
-                  <p class="carousel-item__details--title">Recip name</p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={PhotoFood} alt="" className="carousel-item__img"/>
-                <div class="carousel-item__details">
-                <div className="like-box">
-                  <div className="like" id="like"></div>
-                </div>
-                  <p class="carousel-item__details--title">Recip name</p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={PhotoFood} alt="" className="carousel-item__img"/>
-                <div class="carousel-item__details">
-                <div className="like-box">
-                  <div className="like" id="like"></div>
-                </div>
-                  <p class="carousel-item__details--title">Recip name</p>
-                </div>
-              </div>
-              
-              
+              <CarruselItems />
             </div>
           </section>
-      
         </section>
       );
-    }
-  }
+}
+}
+   
 export default CarruselHome;
