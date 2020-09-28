5aaa45303a3655154371fdb2d18289319354e191
jose caicedo <joseluiscl@gmail.com>
2020-09-27 21:15:41 -0500

    cooktime animation added on RecipeCardSquare component

--- re-z/src/components/RecipeCardSquare.js
+++ re-z/src/components/RecipeCardSquare.js
@@ -1,31 +1,23 @@
 import React from 'react';
 
 import './styles/RecipeCardSquare.css'
-import timeIcon from '../images/Atomos/time-icon-yellow.svg'
-import fotoComida3 from '../images/fotoComida3.png'
+import CookTime from '../Animation/CookTime';
+// import fotoComida3 from '../images/fotoComida3.png'
 
-class RecipeCardCuadradoInfo extends React.Component{
-    render(){
-        return <button className="cardcuadro">
-        <div className="cardcuadro__image">
-            <img src={fotoComida3}  alt="RecipentCardFood"/>
+const RecipeCardCuadradoInfo =({name,picture, preparationTime})=>{
+    return <button className="cardcuadro">
+    <img className="cardcuadro__image" src={picture}  alt="RecipentCardFood"/>
+    <div className="cardcuadro__text">
+        <h1>{name}</h1>
+        <p className='cardcuadro__text_paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Accusantium pariatur quia reprehenderit, distinctio quidem aut sed officia? Omnis qui voluptates 
+        eaque quia sequi? Quasi sit molestias minima nihil autem repellat.</p>
+        <div className="cardcuadro__time">
+            <CookTime />
+            <div className='cardcuadro__time--text'>{preparationTime} min</div>
         </div>
-        <div className="cardcuadro__text">
-            <div className="cardcuadro__text--title">
-                <div>Lorem Ipsum 20</div>
-                <div className='cardcuadro__text_paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Accusantium pariatur quia reprehenderit, distinctio quidem aut sed officia? Omnis qui voluptates 
-                eaque quia sequi? Quasi sit molestias minima nihil autem repellat.</div>
-            </div>
-            <div className="cardcuadro__time">
-                <div className="cardcuadro__time--picture">
-                    <img src={timeIcon} alt="tiempo preparación"/>
-                    <div className='cardcuadro__time--text'>70 min</div>
-                </div>
-            </div>
-        </div>
-        </button>;
-    }
+    </div>
+    </button>;
 }
 
 export default RecipeCardCuadradoInfo;
