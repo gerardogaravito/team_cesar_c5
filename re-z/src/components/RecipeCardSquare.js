238d71532c4de9917b69804dd4cb1a3bf4aebede
jose caicedo <joseluiscl@gmail.com>
2020-09-27 22:40:02 -0500

    Fix RecipeCardSquare Styles

--- re-z/src/components/RecipeCardSquare.js
+++ re-z/src/components/RecipeCardSquare.js
@@ -4,20 +4,20 @@ import './styles/RecipeCardSquare.css'
 import CookTime from '../Animation/CookTime';
 // import fotoComida3 from '../images/fotoComida3.png'
 
-const RecipeCardCuadradoInfo =({name,picture, preparationTime})=>{
-    return <button className="cardcuadro">
-    <img className="cardcuadro__image" src={picture}  alt="RecipentCardFood"/>
-    <div className="cardcuadro__text">
+const RecipeCardSquare =({name,picture, preparationTime})=>{
+    return <button className="CardSquare">
+    <img className="CardSquare__image" src={picture}  alt="RecipentCardFood"/>
+    <div className="CardSquare__text">
         <h1>{name}</h1>
-        <p className='cardcuadro__text_paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        <p className='CardSquare__text_paragraph'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
         Accusantium pariatur quia reprehenderit, distinctio quidem aut sed officia? Omnis qui voluptates 
         eaque quia sequi? Quasi sit molestias minima nihil autem repellat.</p>
-        <div className="cardcuadro__time">
+        <div className="CardSquare__time">
             <CookTime />
-            <div className='cardcuadro__time--text'>{preparationTime} min</div>
+            <div className='CardSquare__time--text'>{preparationTime} min</div>
         </div>
     </div>
     </button>;
 }
 
-export default RecipeCardCuadradoInfo;
+export default RecipeCardSquare;
