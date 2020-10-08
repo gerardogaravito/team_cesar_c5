a3c83b097d002d56995b29dc7f51fc4db40a3053
Dimanson perez <dimaps716@gmail.com>
2020-10-07 23:17:53 -0500

    correccion

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/c/Users/MARIA ANGELICA/Cursos_DIMA/proyecto_platzi_master/proyect/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
